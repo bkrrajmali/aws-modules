c8a1c29daff30a9caf779f934351f72cc0037780
bkrrajmali <bkrraj@gmail.com>
2025-01-01 14:36:53 +0530

    Update outputs.tf

--- vpc/outputs.tf
+++ vpc/outputs.tf
@@ -1,3 +1,19 @@
 output "vpc_id" {
-  value = aws_vpc.main.id
+  value = module.vpc.vpc_id
+}
+
+output "public_subnet_id" {
+  value = module.public_subnet.subnet_id
+}
+
+output "private_subnet_id" {
+  value = module.private_subnet.subnet_id
+}
+
+output "security_group_id" {
+  value = module.security_group.security_group_id
+}
+
+output "ec2_instance_public_ip" {
+  value = module.ec2.public_ip
 }
