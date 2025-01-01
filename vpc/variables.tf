afdade44a68af7cd5cb68269a2e6575aeb5a6f65
bkrrajmali <bkrraj@gmail.com>
2025-01-01 14:47:10 +0530

    Update variables.tf

--- vpc/variables.tf
+++ vpc/variables.tf
@@ -1,10 +1,17 @@
-variable "cidr_block" {}
+variable "cidr_block" {
+  description = "The CIDR block for the VPC"
+  type        = string
+}
+
+variable "tags" {
+  description = "A map of tags to assign to resources"
+  type        = map(string)
+  default     = {}
+}
+
 variable "enable_dns_support" {
   default = true
 }
 variable "enable_dns_hostnames" {
   default = true
-}
-variable "tags" {
-  default = {}
 }
