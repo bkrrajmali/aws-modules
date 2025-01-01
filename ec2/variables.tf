212103cec2515590b2edf4e9405ed0f1446f9466
bkrrajmali <bkrraj@gmail.com>
2025-01-01 14:48:44 +0530

    Update variables.tf

--- ec2/variables.tf
+++ ec2/variables.tf
@@ -1,7 +1,25 @@
-variable "ami" {}
-variable "instance_type" {}
-variable "subnet_id" {}
-variable "security_groups" {}
+variable "ami" {
+  description = "The AMI ID for the EC2 instance"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "The instance type for the EC2 instance"
+  type        = string
+}
+
+variable "subnet_id" {
+  description = "The subnet ID where the EC2 instance will be launched"
+  type        = string
+}
+
+variable "security_groups" {
+  description = "A list of security group IDs to attach to the EC2 instance"
+  type        = list(string)
+}
+
 variable "tags" {
-  default = {}
+  description = "A map of tags to assign to the EC2 instance"
+  type        = map(string)
+  default     = {}
 }
