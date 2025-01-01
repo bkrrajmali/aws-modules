5543179c806fd479519540ba7550729c8b66778f
bkrrajmali <bkrraj@gmail.com>
2025-01-01 15:20:44 +0530

    Update main.tf

--- vpc/main.tf
+++ vpc/main.tf
@@ -1,7 +1,7 @@
 resource "aws_vpc" "main" {
   cidr_block           = var.cidr_block
-  enable_dns_support   = var.enable_dns_support
-  enable_dns_hostnames = var.enable_dns_hostnames
+  enable_dns_support   = true
+  enable_dns_hostnames = true
   tags = var.tags
 }
 
