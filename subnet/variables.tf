42ba0f30a4b35c2b97e668ad8b2f90d50e42121f
bkrrajmali <bkrraj@gmail.com>
2025-01-01 15:12:21 +0530

    Update variables.tf

--- subnet/variables.tf
+++ subnet/variables.tf
@@ -1,9 +1,21 @@
-variable "vpc_id" {}
-variable "cidr_block" {}
-variable "availability_zone" {}
-variable "map_public_ip_on_launch" {
-  default = false
+variable "vpc_id" {
+  description = "The VPC ID to associate with the subnet"
+  type        = string
 }
+
+variable "cidr_block" {
+  description = "The CIDR block for the subnet"
+  type        = string
+}
+
+variable "map_public_ip_on_launch" {
+  description = "Should public IPs be auto-assigned to instances in this subnet"
+  type        = bool
+  default     = false
+}
+
 variable "tags" {
-  default = {}
+  description = "A map of tags to assign to the subnet"
+  type        = map(string)
+  default     = {}
 }
