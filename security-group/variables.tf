47ba140f1481975aac23c9854273c41470a48cb5
bkrrajmali <bkrraj@gmail.com>
2025-01-01 15:13:13 +0530

    Update variables.tf

--- security-group/variables.tf
+++ security-group/variables.tf
@@ -1,12 +1,30 @@
-variable "name" {}
-variable "description" {}
-variable "vpc_id" {}
-variable "ingress_from_port" {}
-variable "ingress_to_port" {}
-variable "ingress_protocol" {}
-variable "ingress_cidr_blocks" {
-  default = ["0.0.0.0/0"]
+variable "vpc_id" {
+  description = "The VPC ID to associate with the security group"
+  type        = string
 }
+
+variable "ingress_from_port" {
+  description = "The starting port for ingress rules"
+  type        = number
+}
+
+variable "ingress_to_port" {
+  description = "The ending port for ingress rules"
+  type        = number
+}
+
+variable "ingress_protocol" {
+  description = "The protocol for ingress rules"
+  type        = string
+}
+
+variable "ingress_cidr_blocks" {
+  description = "The CIDR blocks for ingress rules"
+  type        = list(string)
+}
+
 variable "tags" {
-  default = {}
+  description = "A map of tags to assign to the security group"
+  type        = map(string)
+  default     = {}
 }
